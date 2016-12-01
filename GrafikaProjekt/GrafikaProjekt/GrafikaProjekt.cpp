9938a0a7f0d46626eba553dc1ac137d3313af59f
Adrian <209865@student.pwr.edu.pl>
2016-12-01 13:02:23 +0100

    poprawki

--- GrafikaProjekt/GrafikaProjekt/GrafikaProjekt.cpp
+++ GrafikaProjekt/GrafikaProjekt/GrafikaProjekt.cpp
@@ -1,7 +1,7 @@
-﻿//*************************************************************************/
+﻿//************************************************************************//
 //******					Adrian Frydmański						******//
 //******						209865								******//
-//*************************************************************************/
+//************************************************************************//
 
 #include "stdafx.h"											// odpowiednie biblioteki w nagłówku
 
@@ -37,7 +37,7 @@ int inters;
 float inters_c[3];
 float kon[] = { 0.0, 0.0, 0.0 };
 float reflection_vector[3];
-float normal_vector[3];										
+float normal_vector[3];
 float reflection[3];
 int typ = -1;
 GLubyte pixel[1][1][3];
@@ -52,13 +52,13 @@ void Normal(point p)
 	float d = 0.0;
 	int i;
 
-	for (i = 0; i<3; i++)
+	for (i = 0; i < 3; i++)
 		d += p[i] * p[i];
 
 	d = sqrt(d);
 
-	if (d>0.0)
-		for (i = 0; i<3; i++)
+	if (d > 0.0)
+		for (i = 0; i < 3; i++)
 			p[i] /= d;
 }
 
@@ -129,7 +129,9 @@ void Reflect(float *normal_vector, float *v)
 {
 	float il;
 	float invert[3];
-	invert[0] = -v[0]; invert[1] = -v[1]; invert[2] = -v[2];
+	invert[0] = -v[0]; 
+	invert[1] = -v[1]; 
+	invert[2] = -v[2];
 	Normal(invert);
 	il = scalar(invert, normal_vector);
 	reflection[0] = 2 * (il)*normal_vector[0] - invert[0];
@@ -146,7 +148,8 @@ int Intercest(float *p, float *v, int typ)
 	status = -1;
 	min = 0;
 	float tmp[3];
-	for (int i = 0; i < spheres.size(); i++) {
+	for (int i = 0; i < spheres.size(); i++) 
+	{
 		if (i != typ)
 		{
 			r = -2.0;
@@ -154,25 +157,27 @@ int Intercest(float *p, float *v, int typ)
 			b = 2 * (p[0] * v[0] + p[1] * v[1] + p[2] * v[2] - spheres[i].position[0] * v[0] - spheres[i].position[1] * v[1] - spheres[i].position[2] * v[2]);
 			c = p[0] * p[0] + p[1] * p[1] + p[2] * p[2] - 2 * (spheres[i].position[0] * p[0] + spheres[i].position[1] * p[1] + spheres[i].position[2] * p[2]) + spheres[i].position[0] * spheres[i].position[0] + spheres[i].position[1] * spheres[i].position[1] + spheres[i].position[2] * spheres[i].position[2] - spheres[i].radius * spheres[i].radius;
 			d = b*b - 4 * a*c;
-			if (d >= 0) {
+			if (d >= 0) 
+			{
 				pier = sqrt(d);
 				r1 = (-b - pier) / (2 * a);
 				r2 = (-b + pier) / (2 * a);
 				if (d == 0 && r1 >= 0)
 					r = r1;
 				else
-					if (r1>0 || r2>0)
-						if (r1>r2)
-							if (r2>0)
-								r = r2;
+					if (r1 > 0 || r2 > 0)
+						if (r1 > r2)
+							if (r2 > 0)
+								r = r2 > 0 ? r2 : r1;
 							else
 								r = r1;
 						else
-							if (r1>0)
+							if (r1 > 0)
 								r = r1;
 							else
 								r = r2;
-				if (r>0) {
+				if (r > 0) 
+				{
 					tmp[0] = p[0] + r*v[0];
 					tmp[1] = p[1] + r*v[1];
 					tmp[2] = p[2] + r*v[2];
@@ -383,7 +388,7 @@ void pobierzDane(string name)
 				plik >> liczba;
 				s.radius = liczba;									// radius
 				cout << "Sphere " << spheres.size() + 1 << ": r=" << s.radius << " p=(";
-				for (int i = 0; i < 3; i++) 
+				for (int i = 0; i < 3; i++)
 				{
 					plik >> liczba;
 					s.position.push_back(liczba);					// position
@@ -392,7 +397,7 @@ void pobierzDane(string name)
 						cout << ",";
 				}
 				cout << ") s=(";
-				for (int i = 0; i<3; i++) 
+				for (int i = 0; i < 3; i++)
 				{
 					plik >> liczba;
 					s.specular.push_back(liczba);					// specular
@@ -401,7 +406,7 @@ void pobierzDane(string name)
 						cout << ",";
 				}
 				cout << ") d=(";
-				for (int i = 0; i < 3; i++) 
+				for (int i = 0; i < 3; i++)
 				{
 					plik >> liczba;
 					s.diffuse.push_back(liczba);					// diffuse
@@ -410,7 +415,7 @@ void pobierzDane(string name)
 						cout << ",";
 				}
 				cout << ") a=(";
-				for (int i = 0; i < 3; i++) 
+				for (int i = 0; i < 3; i++)
 				{
 					plik >> liczba;
 					s.ambient.push_back(liczba);					// ambient
@@ -423,11 +428,11 @@ void pobierzDane(string name)
 				cout << ") ss=" << s.specularshininess << "\n";
 				spheres.push_back(s);								// dodaj sferę do vectora
 			}
-			else if (buf[1] == 'o') 
+			else if (buf[1] == 'o')
 			{
 				source s;
-				cout << "Source " << sources.size()+1 << ": p = (";
-				for (int i = 0; i < 3; i++) 
+				cout << "Source " << sources.size() + 1 << ": p = (";
+				for (int i = 0; i < 3; i++)
 				{
 					plik >> liczba;
 					s.position.push_back(liczba);					// position
@@ -436,7 +441,7 @@ void pobierzDane(string name)
 						cout << ",";
 				}
 				cout << ") s=(";
-				for (int i = 0; i < 3; i++) 
+				for (int i = 0; i < 3; i++)
 				{
 					plik >> liczba;
 					s.specular.push_back(liczba);					// specular
@@ -445,7 +450,7 @@ void pobierzDane(string name)
 						cout << ",";
 				}
 				cout << ") d=(";
-				for (int i = 0; i < 3; i++) 
+				for (int i = 0; i < 3; i++)
 				{
 					plik >> liczba;
 					s.diffuse.push_back(liczba);					// diffuse
@@ -454,7 +459,7 @@ void pobierzDane(string name)
 						cout << ",";
 				}
 				cout << ") a=(";
-				for (int i = 0; i < 3; i++) 
+				for (int i = 0; i < 3; i++)
 				{
 					plik >> liczba;
 					s.ambient.push_back(liczba);					// ambient
